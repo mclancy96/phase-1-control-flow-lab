5d092423a602d5fbec0e12a27893ad6d825367f6
Mike <mclancy96@gmail.com>
2025-05-05 13:21:55 -0400

    Make ternaryCheckCity pass tests

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ function scuberGreetingForFeet(rideLength) {
   }
 }
 
-function ternaryCheckCity() {
-  // Write your code here!
+function ternaryCheckCity(city) {
+  return city === 'NYC' ? "Ok, sounds good." : "No go.";
 }
 
 function switchOnCharmFromTip() {
