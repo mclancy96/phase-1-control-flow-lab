df66477973f3c403de4a0756beface39964e07ed
Mike <mclancy96@gmail.com>
2025-05-05 13:19:45 -0400

    Make scuberGreetingForFeet pass tests

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet() {
+  if (rideLength > 2500) {
+    return 'No can do.';
+  } else if (rideLength > 2000) {
+    return 'I will gladly take your thirty bucks.'
+  } else if (rideLength > 400) {
+    return 'That will be twenty bucks.';
+  } else {
+    return 'This one is on me!';
+  }
+}
+
+function ternaryCheckCity() {
   // Write your code here!
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip() {
   // Write your code here!
 }
